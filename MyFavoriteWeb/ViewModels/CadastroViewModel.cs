912c0732fdee1af5f17baf2cf13d688bd144bde4
Danilo Oliveira <danilotecoliveira@yahoo.com.br>
2018-01-04 23:54:48 -0200

    MVVM da página de cadastro

---
+++ MyFavoriteWeb/ViewModels/CadastroViewModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using Windows.Foundation;
+using MyFavoriteWeb.Models;
+using Windows.Media.Capture;
+using MyFavoriteWeb.Services;
+using Windows.UI.Xaml.Media.Imaging;
+using MyFavoriteWeb.Models.Singletons;
+
+namespace MyFavoriteWeb.ViewModels
+{
+    public class CadastroViewModel : BaseModel
+    {
+        private string email;
+        private string senha;
+        private string nome;
+
+        public void Initialize()
+        {
+            if (!string.IsNullOrWhiteSpace(UsuarioLogado.Nome))
+            {
+                Nome = UsuarioLogado.Nome;
+                Email = UsuarioLogado.Email;
+                Senha = UsuarioLogado.Senha;
+            }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { Set(ref email, value); }
+        }
+
+        public string Senha
+        {
+            get { return senha; }
+            set { Set(ref senha, value); }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { Set(ref nome, value); }
+        }
+
+        public void Cancelar()
+        {
+            NavigationService.GoBack();
+        }
+
+        public void Salvar()
+        {
+            try
+            {
+                VerificarUsuarioExistente(email);
+
+                using (var context = new MyAppContext())
+                {
+                    var usuario = new Usuario
+                    {
+                        Nome = nome,
+                        Email = email,
+                        Senha = senha
+                    };
+
+                    context.Usuarios.Add(usuario);
+                    context.SaveChanges();
+                }
+
+                NotificationService.ShowToastNotification("Sucesso", "O cadastro foi realizado com sucesso.");
+
+                NavigationService.Navigate<Login>();
+            }
+            catch (Exception ex)
+            {
+                NotificationService.ShowToastNotification("Erro", ex.Message);
+            }
+        }
+
+        private void VerificarUsuarioExistente(string email)
+        {
+            using (var context = new MyAppContext())
+            {
+                var usuario = context.Usuarios.Where(m => m.Email == email).FirstOrDefault();
+
+                if (usuario != null)
+                {
+                    context.Usuarios.Remove(usuario);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void AbrirCamera()
+        {
+            var captureUI = new CameraCaptureUI();
+            captureUI.PhotoSettings.AllowCropping = true;
+            captureUI.PhotoSettings.CroppedSizeInPixels = new Size(300, 300);
+            captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+
+            var storageFile = captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo).GetResults();
+
+            if (storageFile == null)
+            {
+                //operação cancelada
+            }
+            else
+            {
+                using (var stream = storageFile.OpenReadAsync().GetResults())
+                {
+                    var bitmap = new BitmapImage();
+                    bitmap.SetSourceAsync(stream).GetResults();
+                    //imgAvatar.Source = bitmap;
+                }
+            }
+        }
+    }
+}
--- MyFavoriteWeb/Views/CadastroView.xaml.cs
+++ MyFavoriteWeb/Views/CadastroView.xaml.cs
@@ -1,57 +1,24 @@
 ﻿using System;
-using System.Linq;
 using Windows.UI.Xaml;
-using Windows.Foundation;
-using MyFavoriteWeb.Models;
-using Windows.Media.Capture;
-using MyFavoriteWeb.Services;
 using Windows.UI.Xaml.Controls;
+using MyFavoriteWeb.ViewModels;
 using Windows.UI.Xaml.Media.Imaging;
-using MyFavoriteWeb.Models.Singletons;
 
 namespace MyFavoriteWeb.Views
 {
     public sealed partial class CadastroView : Page
     {
+        public CadastroViewModel ViewModel { get; } = new CadastroViewModel();
+
         public CadastroView()
         {
             InitializeComponent();
-
-            if (!string.IsNullOrWhiteSpace(UsuarioLogado.Nome))
-            {
-                Nome.Text = UsuarioLogado.Nome;
-                Email.Text = UsuarioLogado.Email;
-                Senha.Password = UsuarioLogado.Senha;
-            }
+            Loaded += Login_Loaded;
         }
 
-        private void Cancel(object sender, RoutedEventArgs e)
+        private void Login_Loaded(object sender, RoutedEventArgs e)
         {
-            NavigationService.GoBack();
-        }
-
-        private async void OpenCamera(object sender, RoutedEventArgs e)
-        {
-            var captureUI = new CameraCaptureUI();
-            captureUI.PhotoSettings.AllowCropping = true;
-            captureUI.PhotoSettings.CroppedSizeInPixels = new Size(300, 300);
-            captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
-
-            var storageFile = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
-
-            if (storageFile == null)
-            {
-                //operação cancelada
-            }
-            else
-            {
-                using (var stream = await storageFile.OpenReadAsync())
-                {
-                    var bitmap = new BitmapImage();
-                    await bitmap.SetSourceAsync(stream);
-                    imgAvatar.Source = bitmap;
-                }
-            }
+            ViewModel.Initialize();
         }
 
         private void imgFoto_ImageFailed(object sender, ExceptionRoutedEventArgs e)
@@ -60,49 +27,6 @@ namespace MyFavoriteWeb.Views
             var fallbackImage = new BitmapImage(new Uri("ms-appx:///Assets/StoreLogo.png"));
             img.Width = 100;
             img.Source = fallbackImage;
-        }
-
-        protected void Save(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                VerificarUsuarioExistente(Email.Text);
-
-                using (var context = new MyAppContext())
-                {
-                    var usuario = new Usuario
-                    {
-                        Nome = Nome.Text,
-                        Email = Email.Text,
-                        Senha = Senha.Password
-                    };
-
-                    context.Usuarios.Add(usuario);
-                    context.SaveChanges();
-                }
-
-                NotificationService.ShowToastNotification("Sucesso", "O cadastro foi realizado com sucesso.");
-
-                NavigationService.Navigate<Login>();
-            }
-            catch (Exception ex)
-            {
-                NotificationService.ShowToastNotification("Erro", ex.Message);
-            }
-        }
-
-        private void VerificarUsuarioExistente(string email)
-        {
-            using (var context = new MyAppContext())
-            {
-                var usuario = context.Usuarios.Where(m => m.Email == email).FirstOrDefault();
-
-                if (usuario != null)
-                {
-                    context.Usuarios.Remove(usuario);
-                    context.SaveChanges();
-                }
-            }
         }
     }
 }
