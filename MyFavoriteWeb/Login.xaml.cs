76d2150f4d559551c25a28cfff728c6f47d27600
Danilo Oliveira <danilotecoliveira@yahoo.com.br>
2018-01-04 22:42:55 -0200

    MVVM do Login

--- MyFavoriteWeb/Login.xaml.cs
+++ MyFavoriteWeb/Login.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System.Linq;
-using Windows.UI.Xaml;
-using MyFavoriteWeb.Models;
-using MyFavoriteWeb.Services;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using MyFavoriteWeb.Models.Singletons;
 using MyFavoriteWeb.ViewModels;
 
 namespace MyFavoriteWeb
@@ -21,22 +17,6 @@ namespace MyFavoriteWeb
         private void Login_Loaded(object sender, RoutedEventArgs e)
         {
             ViewModel.Initialize();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            using (var context = new MyAppContext())
-            {
-                var usuario = context.Usuarios.Where(m => m.Email == txtEmail.Text && m.Senha == txtSenha.Password).FirstOrDefault();
-
-                if (usuario == null)
-                    NotificationService.ShowToastNotification("Erro", "Email e/ou Senha incorreto(s).");
-                else
-                {
-                    new UsuarioLogado(usuario);
-                    NavigationService.Navigate<MainPage>();
-                }
-            }
         }
     }
 }
--- MyFavoriteWeb/ViewModels/LoginViewModel.cs
+++ MyFavoriteWeb/ViewModels/LoginViewModel.cs
@@ -1,12 +1,28 @@
-﻿using MyFavoriteWeb.Services;
+﻿using System.Linq;
+using MyFavoriteWeb.Models;
+using MyFavoriteWeb.Services;
+using MyFavoriteWeb.Models.Singletons;
 
 namespace MyFavoriteWeb.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : BaseModel
     {
-        public void Initialize()
-        {
+        private string email;
+        private string senha;
 
+        public void Initialize()
+        { }
+
+        public string Email
+        {
+            get { return email; }
+            set { Set(ref email, value); }
+        }
+        
+        public string Senha
+        {
+            get { return senha; }
+            set { Set(ref senha, value); }
         }
 
         public void NovoCadastro()
@@ -17,6 +33,22 @@ namespace MyFavoriteWeb.ViewModels
         public void Sobre()
         {
             NavigationService.Navigate<Views.SobreView>();
+        }
+
+        public void Logar()
+        {
+            using (var context = new MyAppContext())
+            {
+                var usuario = context.Usuarios.Where(m => m.Email == email && m.Senha == Senha).FirstOrDefault();
+
+                if (usuario == null)
+                    NotificationService.ShowToastNotification("Erro", "Email e/ou Senha incorreto(s).");
+                else
+                {
+                    new UsuarioLogado(usuario);
+                    NavigationService.Navigate<MainPage>();
+                }
+            }
         }
     }
 }
