40f846275e423e44c87218e802a4422370318b87
Danilo Oliveira <danilotecoliveira@yahoo.com.br>
2018-01-10 22:25:19 -0200

    Implementação da função de excluir todos os sites e MVVM da página de Lista de Sites

---
+++ MyFavoriteWeb/ViewModels/ListaSitesViewModel.cs
@@ -0,0 +1,105 @@
+﻿using System.Linq;
+using Xamarin.Forms;
+using Windows.Storage;
+using MyFavoriteWeb.Views;
+using System.Windows.Input;
+using MyFavoriteWeb.Models;
+using MyFavoriteWeb.Services;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using MyFavoriteWeb.Models.Singletons;
+
+namespace MyFavoriteWeb.ViewModels
+{
+    public class ListaSitesViewModel
+    {
+        public string ItemClicado;
+        public ObservableCollection<Site> ListaSites { get; set; } = new ObservableCollection<Site>();
+        public ICommand Visitar { get; set; }
+        public ICommand Excluir { get; set; }
+        public ICommand ExcluirTodos { get; set; }
+
+        internal void Initialize()
+        {
+            CarregaListaSites();
+
+            Visitar = new Command(VisitarSite);
+            Excluir = new Command(ExcluirSite);
+            ExcluirTodos = new Command(ExcluirTodosSites);
+        }
+
+        private void ExcluirTodosSites()
+        {
+            foreach (var item in ListaSites.ToList())
+            {
+                using (var context = new MyAppContext())
+                {
+                    ListaSites.Remove(item);
+                    context.Sites.Remove(item);
+                    context.SaveChanges();
+                }
+            }
+
+            ItemClicado = string.Empty;
+        }
+
+        private void ExcluirSite()
+        {
+            foreach (var item in ListaSites.ToList())
+            {
+                if (item.Id.ToString() == ItemClicado)
+                {
+                    using (var context = new MyAppContext())
+                    {
+                        ListaSites.Remove(item);
+                        context.Sites.Remove(item);
+                        context.SaveChanges();
+                    }
+                }
+            }
+
+            ItemClicado = string.Empty;
+        }
+
+        private void VisitarSite()
+        {
+            foreach (var item in ListaSites.ToList())
+            {
+                if (item.Id.ToString() == ItemClicado)
+                {
+                    WebViewUrl.Url = item.Url;
+                    NavigationService.Navigate<NavegadorWebView>();
+                }
+            }
+
+            ItemClicado = string.Empty;
+        }
+
+        private void CarregaListaSites()
+        {
+            using (var context = new MyAppContext())
+            {
+                var sites = context.Sites.ToList();
+                sites.ForEach(l => l.Imagem = PegarPrint(l.Imagem));
+
+                ListaSites = ConverterLista(sites);
+            }
+        }
+
+        private ObservableCollection<Site> ConverterLista(List<Site> sites)
+        {
+            foreach (var item in sites)
+            {
+                ListaSites.Add(item);
+            }
+
+            return ListaSites;
+        }
+
+        private string PegarPrint(string nomeImagem)
+        {
+            var myfolder = ApplicationData.Current.LocalFolder;
+            return $"{myfolder.Path}/{nomeImagem}";
+        }
+    }
+}
--- MyFavoriteWeb/Views/ListaSitesView.xaml.cs
+++ MyFavoriteWeb/Views/ListaSitesView.xaml.cs
@@ -1,49 +1,25 @@
-﻿using System.Linq;
-using Windows.Storage;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using MyFavoriteWeb.Models;
 using Windows.UI.Xaml.Input;
-using MyFavoriteWeb.Services;
 using Windows.UI.Xaml.Controls;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using MyFavoriteWeb.Models.Singletons;
+using MyFavoriteWeb.ViewModels;
 
 namespace MyFavoriteWeb.Views
 {
     public sealed partial class ListaSitesView : Page
     {
-        private ObservableCollection<Site> lista = new ObservableCollection<Site>();
-        private string itemClicado;
+        public ListaSitesViewModel ViewModel { get; } = new ListaSitesViewModel();
 
         public ListaSitesView()
         {
             InitializeComponent();
-
-            using (var context = new MyAppContext())
-            {
-                var sites = context.Sites.ToList();
-                sites.ForEach(l => l.Imagem = PegarPrint(l.Imagem));
-
-                listaSites.ItemsSource = ConverterLista(sites);
-            }
+            Loaded += ListaSitesView_Loaded;
+            DataContext = ViewModel;
         }
 
-        private ObservableCollection<Site> ConverterLista(List<Site> lista)
+        private void ListaSitesView_Loaded(object sender, RoutedEventArgs e)
         {
-
-            foreach (var item in lista)
-            {
-                this.lista.Add(item);
-            }
-
-            return this.lista;
-        }
-
-        private string PegarPrint(string nomeImagem)
-        {
-            var myfolder = ApplicationData.Current.LocalFolder;
-            return $"{myfolder.Path}/{nomeImagem}";
+            ViewModel.Initialize();
         }
 
         private void listaSites_Tapped(object sender, TappedRoutedEventArgs e)
@@ -51,39 +27,7 @@ namespace MyFavoriteWeb.Views
             var listView = (ListView)sender;
             sitesCadastrados.ShowAt(listView, e.GetPosition(listView));
             var a = ((FrameworkElement)e.OriginalSource).DataContext as Site;
-            itemClicado = a.Id.ToString();
-        }
-
-        private void Remove_Click(object sender, RoutedEventArgs e)
-        {
-            foreach (var item in lista.ToList())
-            {
-                if (item.Id.ToString() == itemClicado)
-                {
-                    using (var context = new MyAppContext())
-                    {
-                        lista.Remove(item);
-                        context.Sites.Remove(item);
-                        context.SaveChanges();
-                    }
-                }
-            }
-
-            itemClicado = string.Empty;
-        }
-
-        private void Visitar_Click(object sender, RoutedEventArgs e)
-        {
-            foreach (var item in lista.ToList())
-            {
-                if (item.Id.ToString() == itemClicado)
-                {
-                    WebViewUrl.Url = item.Url;
-                    NavigationService.Navigate<NavegadorWebView>();
-                }
-            }
-
-            itemClicado = string.Empty;
+            ViewModel.ItemClicado = a.Id.ToString();
         }
     }
 }
